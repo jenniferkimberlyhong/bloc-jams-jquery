ab924bf7c6accf56e8b05d903d2d0380b98003a3
Jennifer Hong <jkhongx@gmail.com>
2019-04-07 17:53:51 +0000

    Update Player Bar

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -7,7 +7,7 @@
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
-    const currentSongIndex = album.songs.indexOf(player.currentPlaying);
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
 
@@ -15,11 +15,29 @@
     player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+}
+
+$('#volume-control input').on('input' function (event) {
+  player.setVolume(event.target.value);
+});
+
 $('button#previous').on('click', function () {
   if (player.playState !== 'playing') { return; }
 
-  const currentSongIndex = album.songs.indexOf(player.currentPlaying);
-  const previousSongIndex = previousSongIndex - 1;
+  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  const previousSongIndex = currentSongIndex - 1;
   if (previousSongIndex < 0) { return; }
 
   const previousSong = album.songs[previousSongIndex];
